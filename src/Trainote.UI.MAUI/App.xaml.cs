9cf9bb4e195db9bb00405ca0eaaaedbf423fa6e0
Michal Ogluszka <michal.ogluszka@outlook.com>
2022-11-05 22:01:54 +0100

    hello world

---
+++ src/Trainote.UI.MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Trainote.UI.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/Trainote.UI.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Trainote.UI.MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
